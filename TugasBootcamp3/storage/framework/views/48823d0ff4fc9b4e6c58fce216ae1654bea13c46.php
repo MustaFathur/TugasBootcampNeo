7b2f4cefa113e7f73e01d1f9a2cf140ebcb895c3
Mustafa Fathur Rahman <139726095+MustaFathur@users.noreply.github.com>
2023-12-20 23:59:01 +0700

    Add files via upload

---
+++ TugasBootcamp3/storage/framework/views/48823d0ff4fc9b4e6c58fce216ae1654bea13c46.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Tugas Bootcamp 3 | <?php echo $__env->yieldContent('title'); ?></title>
+</head>
+<body>
+  <?php $__env->startSection('navbar'); ?>
+    <?php echo $__env->make('layouts.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+  <?php echo $__env->yieldSection(); ?>
+
+
+    <div class="container mt-4">
+        <?php echo $__env->yieldContent('content'); ?>
+
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+      integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+      integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+      crossorigin="anonymous"
+    ></script>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\TugasBootcamp3\resources\views/layouts/app.blade.php ENDPATH**/ ?>
