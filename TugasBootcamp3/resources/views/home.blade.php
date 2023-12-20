872b77cb440951cb08fedaa4a0db8d4c97324c8b
Mustafa Fathur Rahman <139726095+MustaFathur@users.noreply.github.com>
2023-12-21 00:52:50 +0700

    Add files via upload

---
+++ TugasBootcamp3/resources/views/home.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.app')
+@section ('title','FathurPin')
+
+@section('navbar')
+@parent
+@endsection
+
+@section ('content')
+<div class="row mt-3">
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="card">
+        <img src="img/MobileLegend.png" class="card-img-top" alt="Game 1" />
+        <div class="card-body">
+          <h5 class="card-title">Mobile Legend</h5>
+          <p class="card-text">Legenda Seluler</p>
+          <a href="#" class="btn btn-primary">Top Up</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="card">
+        <img src="img/genshin.webp" class="card-img-top" alt="Game 2" />
+        <div class="card-body">
+          <h5 class="card-title">Genshin Impact</h5>
+          <p class="card-text">Game wibu</p>
+          <a href="#" class="btn btn-primary">Top Up</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="card">
+        <img src="img/ApiGratis.jpeg" class="card-img-top" alt="Game 3" />
+        <div class="card-body">
+          <h5 class="card-title">Free Fire</h5>
+          <p class="card-text">Free Fire</p>
+          <a href="#" class="btn btn-primary">Top Up</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="card">
+        <img src="img/cod.jpeg" class="card-img-top" alt="Game 4" />
+        <div class="card-body">
+          <h5 class="card-title">Call of Duty Mobile</h5>
+          <p class="card-text">Panggilan Tugas</p>
+          <a href="#" class="btn btn-primary">Top Up</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="card">
+        <img src="img/coc.png" class="card-img-top" alt="Game 5" />
+        <div class="card-body">
+          <h5 class="card-title">Clash of Clans</h5>
+          <p class="card-text">Game Perbudakan</p>
+          <a href="#" class="btn btn-primary">Top Up</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="card">
+        <img src="img/pubg.png" class="card-img-top" alt="Game 6" />
+        <div class="card-body">
+          <h5 class="card-title">PUBG Mobile</h5>
+          <p class="card-text">Hunger Game</p>
+          <a href="#" class="btn btn-primary">Top Up</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="card">
+        <img src="img/ea.png" class="card-img-top" alt="Game 7" />
+        <div class="card-body">
+          <h5 class="card-title">FIFA EA Esport</h5>
+          <p class="card-text">PES Better</p>
+          <a href="#" class="btn btn-primary">Top Up</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+</div>
+</div>
+
+
+@endsection
